e482b831da65add6087a2c518138d40cb438005a
Francisco Orchard <f.orchard@epiconcept.fr>
2018-10-08 14:51:03 +0200

    1. Adding possibility to drop steps to perform a partial execution 2. Change behavior from exception to wanring when applying parameters to multiple or no steps

--- mllib/src/main/scala/ModelVersion.scala
+++ mllib/src/main/scala/ModelVersion.scala
@@ -2,6 +2,7 @@ package demy.mllib
 
 import org.apache.spark.sql.types._
 import org.apache.spark.sql.catalyst.expressions.GenericRowWithSchema
+import demy.mllib.util.log._
 
 case class ModelVersion(steps:Seq[ModelStep]=Seq[ModelStep](), comment:String="", customLogs:GenericRowWithSchema=new GenericRowWithSchema(values= Array[Any](), schema=new StructType(Array[StructField]()))) {
     def isReady() {
@@ -23,20 +24,27 @@ case class ModelVersion(steps:Seq[ModelStep]=Seq[ModelStep](), comment:String=""
         val repeatedChoices = choices.groupBy(c =>c.step).flatMap(p => p match {case (step, choices) => if(choices.size > 1) Some((step, choices.map(c => c.version))) else None})
         if(repeatedChoices.size > 0) throw new Exception(s"cannot choose more than one version (${repeatedChoices.map(rc => rc match {case (step, versions) => s"${step}[${versions.mkString(";")}]"}).mkString("|") })")
         val noMatchs = choices.filter(c => this.steps.filter(s => s.name == c.step && s.version == c.version).size != 1).map(c => c.step)
-        if(noMatchs.size > 0) throw new Exception(s"Step(s) ${noMatchs.mkString(", ")} should exists exactly once among (${this.steps.map(step =>  (step.name, step.version)).mkString})")
+        if(noMatchs.size > 0) warning(s"Step(s) ${noMatchs.mkString(", ")} should exists exactly once among (${this.steps.map(step =>  (step.name, step.version)).mkString})")
         
         val newSteps = this.steps
                             .filter(step => choices.filter(c => c.step == step.name).size == 0 || choices.filter(c => c.step == step.name && c.version == step.version ).size == 1)
-                            .map(step => if(choices.filter(c => c.step == step.name && c.version == step.version ).size == 1) step.logStep else step)
+                            .map(step => if(choices.filter(c => c.step == step.name && c.version == step.version ).size == 1) step.option("log"->"true") else step)
         ModelVersion(steps = newSteps, comment = this.comment, customLogs = this.customLogs)
     }
     def drop(stepsToDrop:String*) = {
         val allSteps = this.steps.map(step => step.name).toSet
         val dropSet = stepsToDrop.toSet
         val nonExisting = dropSet.diff(allSteps)
-        if(nonExisting.size > 0) throw new Exception(s"Cannot fin the following(s) step to drop: ${nonExisting.mkString(",")} between ${allSteps.mkString(",")}")
+        if(nonExisting.size > 0) warning(s"Cannot fin the following(s) step to drop: ${nonExisting.mkString(",")} between ${allSteps.mkString(",")}")
 
         val newSteps = this.steps.filterNot(step => dropSet.contains(step.name))
+        
+        ModelVersion(steps = newSteps, comment = this.comment, customLogs = this.customLogs)
+    }
+    
+    def dropAfter(lastStep:String) = {
+        var afterLast = false
+        val newSteps = this.steps.iterator.takeWhile((step)=>{val isAfter = afterLast; if(step.name==lastStep) {afterLast = true}; !isAfter}).toArray.toSeq
         
         ModelVersion(steps = newSteps, comment = this.comment, customLogs = this.customLogs)
     }
@@ -46,14 +54,14 @@ case class ModelVersion(steps:Seq[ModelStep]=Seq[ModelStep](), comment:String=""
     }
     def params(step:String, paramPairs:(String, Any)*):ModelVersion = {
         val versionNames = this.steps.flatMap(s => if(s.name == step) Some(s.version) else None)
-        if(versionNames.size == 0) throw new Exception(s"Cannot find step ${step}: between [${this.steps.map(step => step.name).mkString(", ")}]")
+        if(versionNames.size == 0) warning(s"Cannot find step ${step}: between [${this.steps.map(step => step.name).mkString(", ")}]")
         versionNames.foldLeft(this)((current, versionName)=>current.params(step, versionName, paramPairs :_*))
     }
     def params(step:String, version:String, paramPairs:(String, Any)*):ModelVersion = {
         val stepParams = paramPairs.map(p => StepParam[Any](path = p._1, value=p._2, log = true))
         val selectedSteps = this.steps.filter(s => s.name == step && s.version == version)        
         if(selectedSteps.size != 1) throw new Exception(s"parameter ($step,$version) should match a single stem instead of ${selectedSteps.size}")
-        val theSteps = this.steps.map(s => if(s.name == step && s.version == version) s.applyParams(stepParams:_*).logStep else s)
+        val theSteps = this.steps.map(s => if(s.name == step && s.version == version) s.applyParams(stepParams:_*).option("log"->"true") else s)
         ModelVersion(steps = theSteps, comment = this.comment, customLogs = this.customLogs)
     }
     def log(toLog:(String,Any)*) = {

7ae548cc8b2ecbe26eb33f0549fb2fa4617326a2
Francisco Orchard <f.orchard@epiconcept.fr>
2018-10-26 03:46:58 +0200

    1. Adding parallel evaluation of folds 2. Adding possibility to expliicity define which lines goes to train and tests

--- mllib/src/main/scala/tuning/FoldsPredictor.scala
+++ mllib/src/main/scala/tuning/FoldsPredictor.scala
@@ -1,29 +1,36 @@
 package demy.mllib.tuning
 
 import demy.mllib.params.HasParallelismDemy
-import demy.mllib.util.log.msg
+import demy.mllib.util.log.{msg, debug}
 import org.apache.spark.ml.{Transformer, Estimator}
 import org.apache.spark.ml.util.Identifiable
 import org.apache.spark.ml.param.{Param, ParamMap, Params}
 import org.apache.spark.sql.{Dataset, DataFrame}
+import org.apache.spark.sql.functions.{lit, expr, not}
 import org.apache.spark.sql.types._
 import org.apache.spark.ml.param.shared._
+import scala.concurrent.{Await, Future, ExecutionContext, blocking}
+import scala.concurrent.duration.Duration
 
 
 abstract class Folder extends Params {
-  def buildFolds(ds:Dataset[_]):Array[(Dataset[_], Dataset[_])]
+  def buildFolds[T](ds:Dataset[T]):Array[(Dataset[T], Dataset[T])]
 }
 
 trait FoldsPredictorBase extends HasParallelismDemy with HasPredictionCol with HasLabelCol with HasRawPredictionCol with HasFeaturesCol {
     val uid: String
     val estimator = new Param[FoldsPredictorBase.EstimatorType](this, "estimator", "the estimator that will build the model")
     val folder = new Param[FoldsPredictorBase.FolderType](this, "folder", "the folder that will define the folds to train and validate the model")
+    val forceTestOn = new Param[String](this, "forceTestOn", "SQL formula for lines that are only going to be used to test")
+    val forceTrainOn = new Param[String](this, "forceTrainOn", "SQL formula for lines that are only going to be used to training")
     def setFeaturesCol(value: String): this.type = set(featuresCol, value)
     def setLabelCol(value: String): this.type = set(labelCol, value)
     def setPredictionCol(value: String): this.type = set(predictionCol, value)
     def setRawPredictionCol(value: String): this.type = set(rawPredictionCol, value)
     def setEstimator(value: FoldsPredictorBase.EstimatorType): this.type = set(estimator, value)
     def setFolder(value: FoldsPredictorBase.FolderType): this.type = set(folder, value)
+    def setForceTestOn(value: String): this.type = set(forceTestOn, value)
+    def setForceTrainOn(value: String): this.type = set(forceTrainOn, value)
     def validateAndTransformSchema(schema: StructType): StructType = get(estimator).get.transformSchema(schema)
 }
 
@@ -59,10 +66,19 @@ class FoldsPredictor(override val uid: String) extends Estimator[FoldsPredictorM
 }
 
 class FoldsPredictorModel(override val uid: String, val model:FoldsPredictorBase.ModelType) extends org.apache.spark.ml.Model[FoldsPredictorModel] with FoldsPredictorBase {
-    override def transform(dataset: Dataset[_]): DataFrame = {
+    override def transform(ds: Dataset[_]): DataFrame = typedTransform(ds)
+    def typedTransform[T](ds: Dataset[T]): DataFrame = {
       val folderTrans = get(folder).get
+      val forcedTrainSet = get(forceTrainOn) match {case Some(trainExp) => Some(ds.where(expr(trainExp))) case _ => None} 
+      val forcedTestSet = get(forceTestOn) match {case Some(testExp) => Some(ds.where(expr(testExp))) case _ => None}
+      val toFoldDs = (get(forceTrainOn), get(forceTestOn)) match {
+                       case (Some(trainExp), Some(testExp)) => ds.where(not(expr(trainExp)) && not(expr(testExp)))
+                       case (Some(trainExp), None) => ds.where(not(expr(trainExp)))
+                       case (None, Some(testExp)) => ds.where(not(expr(testExp)))
+                       case (None, None) => ds
+                     }
       val folds = folderTrans
-            .buildFolds(dataset)
+            .buildFolds(toFoldDs)
       var baseEstimator = get(estimator).get
       baseEstimator = baseEstimator
             .set(baseEstimator.predictionCol, get(predictionCol).get)
@@ -70,13 +86,22 @@ class FoldsPredictorModel(override val uid: String, val model:FoldsPredictorBase
             .set(baseEstimator.featuresCol, get(featuresCol).get)
             .set(baseEstimator.rawPredictionCol, get(rawPredictionCol).get)
 
-      msg("training folds")
-      val predictions = folds.map(fold => fold match { case (train, test) => {
-          val model = baseEstimator.fit(train)
-          val predictions = model.transform(test)
-          predictions
-      }}).reduce((ds1, ds2) => ds1.union(ds2))
-      msg("folds trained")
+      debug("training folds")
+      val executionContext = getExecutionContext
+      val predictionsFutures = folds.map(fold => fold match { case (trainFold, testFold) => {
+        Future {
+            blocking {
+              val trainSet = forcedTrainSet match {case Some(forced) =>trainFold.union(forced) case _ => trainFold }
+              val testSet = forcedTestSet match {case Some(forced) =>testFold.union(forced) case _ => testFold }
+              debug(s"training on ${trainSet.count} and resting on ${testSet.count}")
+              val model = baseEstimator.fit(trainSet)
+              val preds = model.transform(testSet)
+              preds
+            }
+        }(executionContext)
+      }})
+      val predictions = predictionsFutures.map(future => Await.result(future, Duration.Inf)).reduce((ds1, ds2) => ds1.union(ds2))
+      debug("folds trained")
       predictions
     }
     override def transformSchema(schema: StructType): StructType = schema

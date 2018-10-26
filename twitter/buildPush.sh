c3e4b1037c556229ca5e798a2fda80b49b60bdb8
Francisco Orchard <f.orchard@epiconcept.fr>
2018-10-26 03:56:16 +0200

    changing call to client yarn mode to prevent error when using proxy

--- twitter/buildPush.sh
+++ twitter/buildPush.sh
@@ -2,5 +2,5 @@ sbt assembly
 
 scp target/scala-2.11/demy-twitter-track-assembly-1.0.jar sparkrunner:~
 
-ssh sparkrunner 'echo "/space/hadoop/spark_home/bin/spark-submit --class \"twitemy.Execute\" --master yarn --executor-cores 1 --driver-cores 1 --num-executors 1 --deploy-mode cluster --name \"Twitter imports\" --driver-memory 520M --executor-memory 520M \"/home/spark/demy-twitter-track-assembly-1.0.jar\" " > run.sh; chmod 700 run.sh'
+ssh sparkrunner 'echo "nohup /space/hadoop/spark_home/bin/spark-submit --class \"twitemy.Execute\" --master yarn --executor-cores 1 --driver-cores 1 --num-executors 1 --deploy-mode client --name \"Twitter imports\" --driver-memory 520M --executor-memory 520M \"/home/spark/demy-twitter-track-assembly-1.0.jar\" >/dev/null 2>&1 &" > run.sh; chmod 700 run.sh'
 

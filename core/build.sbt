68ce071dd0ab33cf44b8024998894acedbe9c7fb
Francisco Orchard <f.orchard@epiconcept.fr>
2018-11-08 16:26:47 +0100

    Moving ml utils to core

--- core/build.sbt
+++ core/build.sbt
@@ -5,6 +5,8 @@ lazy val root = (project in file(".")).
     version := "1.0",
     libraryDependencies += "org.scalactic" %% "scalactic" % "3.0.5",
     libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.5" % "test",
+    libraryDependencies += "org.apache.spark" %% "spark-core" % "2.3.2" % "provided",
+    libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.3.2" % "provided",
 
     assemblyMergeStrategy in assembly := {
       case PathList("org","aopalliance", xs @ _*) => MergeStrategy.last
--- core/src/main/scala/util/util.scala
+++ core/src/main/scala/util/util.scala
@@ -1,18 +1,27 @@
 package demy.util
 
+
+object util {
+    def checkpoint[T : org.apache.spark.sql.Encoder : scala.reflect.runtime.universe.TypeTag] (ds:org.apache.spark.sql.Dataset[T], path:String)
+               :org.apache.spark.sql.Dataset[T] = 
+    {
+        ds.write.mode("overwrite").parquet(path)
+        return ds.sparkSession.read.parquet(path).as[T]
+    }
+
+    def checkpoint(df:org.apache.spark.sql.DataFrame, path:String) = 
+    {
+        df.write.mode("overwrite").parquet(path)
+        df.sparkSession.read.parquet(path)
+    }
+/*    def schemaOf[T: scala.reflect.runtime.universe.TypeTag]: StructType = { 
+        org.apache.spark.sql.catalyst.ScalaReflection
+            .schemaFor[T] // this method requires a TypeTag for T
+            .dataType
+            .asInstanceOf[StructType] // cast it to a StructType, what spark requires as its Schema
+    }*/
+}
 case class MergedIterator[T, U](a:Iterator[T], b:Iterator[U], defA:T, defB:U) extends Iterator[(T, U)] {
-  def hasNext = a.hasNext || b.hasNext
-  def next = (if(a.hasNext) a.next else defA, if(b.hasNext) b.next else defB)
-}
-
-
-case class EnumerationFromIterator[T](it:Iterator[T]) extends java.util.Enumeration[T] {
-  def hasMoreElements() = it.hasNext
-  def nextElement() = it.next()
-}
-
-object implicits {
-  implicit class IteratorToJava[T](it: Iterator[T]) {
-    def toJavaEnumeration = EnumerationFromIterator(it)
-  }
+      def hasNext = a.hasNext || b.hasNext
+      def next = (if(a.hasNext) a.next else defA, if(b.hasNext) b.next else defB)
 }

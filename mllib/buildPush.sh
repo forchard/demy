334e8fd43b046d7a910468c0b72d8193b39057e4
Francisco Orchard <f.orchard@epiconcept.fr>
2018-11-08 16:21:38 +0100

    Adding missings jars for deployment

--- mllib/buildPush.sh
+++ mllib/buildPush.sh
@@ -1,4 +1,6 @@
 sbt package
 cd ../deploy
-ansible bgdtz2.oxa -b -m copy -a "src=../mllib/target/scala-2.11/demy-machine-learning-library_2.11-1.0.jar dest={{zeppelin_run}}/custom-libs/"
+ansible spark_edge -b -m copy -a "src=../storage/target/scala-2.11/demy-storage-layer_2.11-1.0.jar dest={{zeppelin_run}}/custom-libs/"
+ansible spark_edge -b -m copy -a "src=../core/target/scala-2.11/demy-core_2.11-1.0.jar dest={{zeppelin_run}}/custom-libs/"
+ansible spark_edge -b -m copy -a "src=../mllib/target/scala-2.11/demy-machine-learning-library_2.11-1.0.jar dest={{zeppelin_run}}/custom-libs/"
 cd ../mllib

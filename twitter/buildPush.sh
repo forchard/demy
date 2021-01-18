e0ca77d172df990af80a24c0b1f75821998dec33
Francisco Orchard <f.orchard@epiconcept.fr>
2021-01-18 13:14:28 +0100

    Improving twitter runner deployment script

--- twitter/buildPush.sh
+++ twitter/buildPush.sh
@@ -4,5 +4,5 @@ cd ../deploy
 ansible spark_edge -b -m copy -a "src=../twitter/target/scala-2.11/demy-twitter-track-assembly-1.0.jar dest={{zeppelin_run}}/custom-libs/"
 cd ../twitter
 
-ssh sparkrunner 'echo "nohup /space/hadoop/spark_home/bin/spark-submit --class \"demy.twitter.Execute\" --master yarn --executor-cores 1 --driver-cores 1 --num-executors 1 --deploy-mode client --name \"Twitter imports\" --driver-memory 520M --executor-memory 520M \"/home/spark/demy-twitter-track-assembly-1.0.jar\" >/dev/null 2>&1 &" > run.sh; chmod 700 run.sh'
+ssh sparkrunner 'echo "nohup /space/hadoop/spark_home/bin/spark-submit --class \"demy.twitter.Execute\" --master yarn --executor-cores 1 --driver-cores 1 --num-executors 1 --deploy-mode client --name \"Twitter imports\" --driver-memory 520M --executor-memory 520M \"/space/hadoop/zeppelin_run/custom-libs/demy-twitter-track-assembly-1.0.jar\" >/dev/null 2>&1 &" > run.sh; chmod 700 run.sh'
 
